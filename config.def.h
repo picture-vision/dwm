adc1e13002c3c12627cb8b5742eacd23a44b1e47
BaTemaN <fw@bateman.de>
2021-05-13 10:29:19 +0200

    added dwm-bar-height patch

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Liberation mono:size=12:antialias=true:autohint=true","FiraCode NF:size=16" };
