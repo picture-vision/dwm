6d8be3f60dbd84107cd06b9d408234f524a72fc5
picture-vision <fw@picture-vision.com>
2022-04-17 11:21:22 +0200

    applied rotatestack patch

--- config.def.h
+++ config.def.h
@@ -115,6 +115,8 @@ static Key keys[] = {
 	// { MODKEY,                       XK_s,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY, 						          XK_m,      spawn,          {.v = setmon }},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
