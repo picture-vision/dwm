866e76e45ec26652efd8ba7afeaf87f542bc48b2
FW <fw@picture-vision.com>
2024-03-23 14:10:17 +0100

    patch colorbar

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ rm -f *.rej
 make clean
 make
 sudo make install
-sudo cp ./resources/usr/share/xsessions/dwm.desktop /usr/share/xsessions/
+[ -d /usr/share/xsessions ] && sudo cp ./resources/usr/share/xsessions/dwm.desktop /usr/share/xsessions/
 # sudo cp -f ./scripts/* /usr/local/bin/
